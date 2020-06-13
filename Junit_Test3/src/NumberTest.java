6b677a7cd52afcf0212ab721bd8666b7952cae9f
chohyewon <chohyewon@DESKTOP-1UMJ05V>
2020-06-13 17:48:17 +0900

    commit whyrano..

---
+++ Junit_Test3/src/NumberTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NumberTest {
+
+	  @Test  
+	    public void testNumber() {  
+	        Number num = new Number();  
+	        assertEquals(0, num.getValue());  
+	    }  
+	  
+	    @Test  
+	    public void testNumberInt() {  
+	        Number num = new Number(10);  
+	        assertEquals(10, num.getValue());  
+	    }  
+	  
+	    @Test  
+	    public void testAdd() {  
+	        Number num = new Number(10);  
+	        assertEquals(20, num.add(10));  
+	    }  
+	  
+	    @Test  
+	    public void testMinus() {  
+	        Number num = new Number(10);  
+	        assertEquals(5, num.minus(5));  
+	    }  
+	  
+	    @Test  
+	    public void testMultiply() {  
+	        Number num = new Number(5);  
+	        assertEquals(25, num.multiply(5));  
+	    }  
+	  
+	    @Test  
+	    public void testDivide() {  
+	        Number num = new Number(20);  
+	        assertEquals(2, num.divide(10));  
+	    }  
+	  
+	    @Test  
+	    public void testGetValue() {  
+	        Number num = new Number(20);  
+	        assertEquals(20, num.getValue());  
+	    }  
+	    
+	    
+	    @Test  
+	    public void testMultiply2() {  
+	        Number num = new Number(4);  
+	        assertEquals(16, num.multiply(4));  
+	    }  
+}
